74e38cb97ed99dd57fafd0bd9ade1cb11a44587b
Vitaly Homonov <avg.idm@gmail.com>
2017-07-11 23:05:15 +0300

    change initial state

--- app/src/redux/reducers/Groups.js
+++ app/src/redux/reducers/Groups.js
@@ -31,15 +31,7 @@ import {
 const mock = prepareMock({
   0: { id: 0, color: getColorForGroup(0) },
   1: { id: 1, color: getColorForGroup(1) },
-  2: { id: 2, color: getColorForGroup(2) },
-  3: { id: 3, color: getColorForGroup(3) },
-  4: { id: 4, color: getColorForGroup(4) },
-  5: { id: 5, color: getColorForGroup(5) },
-  6: { id: 6, color: getColorForGroup(6) },
-  7: { id: 7, color: getColorForGroup(7) },
-  8: { id: 8, color: getColorForGroup(8) },
-  9: { id: 9, color: getColorForGroup(9) },
-  10: { id: 10, color: getColorForGroup(10) }
+  2: { id: 2, color: getColorForGroup(2) }
 });
 
 const initialState = fromJS({
--- app/src/redux/reducers/Standards.js
+++ app/src/redux/reducers/Standards.js
@@ -27,9 +27,7 @@ const mock = prepareMock({
   0: { id: 0, x: -5, y: -5, groupId: 0 },
   1: { id: 1, x: 0, y: 0, groupId: 1 },
   2: { id: 2, x: 5, y: 5, groupId: 2 },
-  3: { id: 3, x: 5, y: -5, groupId: 3 },
-  4: { id: 4, x: -5, y: 5, groupId: 4 },
-  5: { id: 5, x: 6, y: 4, groupId: 2 }
+  3: { id: 3, x: 5, y: -5, groupId: 2 }
 });
 
 const initialState = fromJS({
